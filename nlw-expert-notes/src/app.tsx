a9a5a93e73fb4ebe687bc48d5b577d9caca0f634
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-12 00:15:00 -0300

    feat: was added search input with filter for the notes

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -1,7 +1,7 @@
 import logo from './assets/logo-nlw-expert.svg';
 import { NewNoteCard } from './components/new-note-card';
 import { NoteCard } from './components/note-card';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 // The ID is generated with randomUUID, in Math.random use cases the same ID could rarely be generated.
 interface Note {
@@ -11,6 +11,8 @@ interface Note {
 }
 
 export function App() {
+	const [search, setSearch] = useState('');
+
 	// The notes has array format!
 	const [notes, setNotes] = useState<Note[]>(() => {
 		const notesOnStorage = localStorage.getItem('notes');
@@ -32,6 +34,15 @@ export function App() {
 		localStorage.setItem('notes', JSON.stringify(notesArray));
 	}
 
+	function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+		const query = event.target.value;
+
+		setSearch(query);
+	}
+
+	const filteredNotes =
+		search !== '' ? notes.filter((note) => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : notes;
+
 	return (
 		<div className="mx-auto max-w-6xl my-12 space-y-6">
 			{/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
@@ -42,6 +53,7 @@ export function App() {
 					placeholder="Busque em suas notas..."
 					className="w-full bg-transparent text-3xl font-semibold tracking-tight
 					 outline-none placeholder:text-slate-500"
+					onChange={handleSearch}
 				/>
 			</form>
 			<div className="h-px bg-slate-700" />
@@ -51,7 +63,7 @@ export function App() {
 				{/* <NoteCard note={{ date: new Date(), content: 'Hello world' }} /> */}
 
 				{/* The ID (key) is used by React to filter which part of the HTML actually needs to be reloaded. */}
-				{notes.map((note) => {
+				{filteredNotes.map((note) => {
 					return <NoteCard key={note.id} note={note} />;
 				})}
 			</div>
