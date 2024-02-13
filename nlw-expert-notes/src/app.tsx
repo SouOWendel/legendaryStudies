e12dcc62678b369a9d01e915383be9deb2466020
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-12 23:18:28 -0300

    feat: a button and functio to delete notes has been added

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -41,10 +41,21 @@ export function App() {
 	}
 
 	const filteredNotes =
-		search !== '' ? notes.filter((note) => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : notes;
+		search !== ''
+			? notes.filter((note) => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+			: notes;
+
+	function onNoteDeleted(id: string) {
+		const notesArray = notes.filter((note) => {
+			return note.id !== id;
+		});
+
+		setNotes(notesArray);
+		localStorage.setItem('notes', JSON.stringify(notesArray));
+	}
 
 	return (
-		<div className="mx-auto max-w-6xl my-12 space-y-6">
+		<div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
 			{/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
 			<img src={logo} alt="nlw expert" />
 			<form className="w-full">
@@ -57,14 +68,14 @@ export function App() {
 				/>
 			</form>
 			<div className="h-px bg-slate-700" />
-			<div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
 				<NewNoteCard onNoteCreated={onNoteCreated} />
 				{/* two braces, one to use javascript, other is an object */}
 				{/* <NoteCard note={{ date: new Date(), content: 'Hello world' }} /> */}
 
 				{/* The ID (key) is used by React to filter which part of the HTML actually needs to be reloaded. */}
 				{filteredNotes.map((note) => {
-					return <NoteCard key={note.id} note={note} />;
+					return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />;
 				})}
 			</div>
 		</div>
