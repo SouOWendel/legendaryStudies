804102a749de4638e3cc202ddf8d6ed57752511b
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-02-13 01:29:24 -0300

    fix(note-card): some CSS classes was been added (overflow, break-all and whitespace) to prevent the design from breaking

--- nlw-expert-notes/src/components/note-card.tsx
+++ nlw-expert-notes/src/components/note-card.tsx
@@ -23,7 +23,7 @@ export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
 				<span className="text-sm font-medium text-slate-300">
 					{formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
 				</span>
-				<p className="text-sm leading-6 text-slate-400">{note.content}</p>
+				<p className="text-sm leading-6 text-slate-400 break-all">{note.content}</p>
 				<div
 					className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t
 				 from-black/60 to-black/0 pointer-events-none"
@@ -34,16 +34,19 @@ export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
 			<Dialog.Portal>
 				<Dialog.Overlay className="inset-0 fixed bg-black/50" />
 				<Dialog.Content
-					className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2
-					 md:max-w-[648px] md:h-[60vh] w-full bg-slate-700 md:rounded-md flex flex-col outline-none">
+					className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2
+					 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[648px] md:h-[60vh] w-full bg-slate-700 md:rounded-md 
+					 flex flex-col outline-none">
 					<Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400">
 						<X className="size-5 hover:text-slate-100" />
 					</Dialog.Close>
-					<div className="flex flex-1 flex-col gap-3 p-5">
+					<div className="flex flex-1 flex-col gap-3 p-5 overflow-auto">
 						<span className="text-sm font-medium text-slate-300">
 							{formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
 						</span>
-						<p className="text-sm leading-6 text-slate-400">{note.content}</p>
+						<div className="overflow-auto">
+							<p className="text-sm leading-6 text-slate-400 break-all whitespace-pre-line">{note.content}</p>
+						</div>
 					</div>
 					<button
 						type="button"
