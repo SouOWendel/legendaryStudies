958790f64ae6cf072e0f0338a334e7633e356da4
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-08-21 01:15:24 -0300

    feat(pokedex): was relocated the pokemon requests to API to other file and minimized and optimized the pokemon HTML handler

--- javascript-studies/pokedex/assets/js/main.js
+++ javascript-studies/pokedex/assets/js/main.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 function convertPokemonToHtml(pokemon) {
 	return `
 		<li class="pokemon">
@@ -19,24 +15,7 @@ function convertPokemonToHtml(pokemon) {
 	`;
 }
 
-/**
- * O função Fetch para requisição de uma Promise e todo o seu tratamento.
- * Feita a requisição, o método THEN é chamado juntamente a sua resposta
- * que deve vir quando a Promise é aprovada ou rejeitada. Depois disso
- * o código do THEN será executado.
- * 
- * O método CATCH será chamado apenas se a função FETCH retornar um erro.
- * O método FINALLY será chamado assim que a requisição do FETCH por concluída,
- * independente de ser sido aceita ou rejeitada.
- */
-fetch(url)
-	.then((response) => response.json())
-	.then((jsonBody) => jsonBody.results)
-	.then((pokemonList) => {
-		for (const pokemon of pokemonList) {
-			const pokemonOl = document.getElementById('pokemonList');
-			pokemonOl.innerHTML += convertPokemonToHtml(pokemon);
-		}
-	})
-	.catch((error) => console.error(error))
-	.finally(() => console.info('Requisição concluída!'));
+const pokemonOl = document.getElementById('pokemonList');
+pokeApi.getPokemons().then((pokemons = []) => {
+	pokemonOl.innerHTML = pokemons.map(convertPokemonToHtml).join('');
+});
