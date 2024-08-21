916ce80414697b3da76c1e8f7a0214cb2160cf9d
Wendel Henrique (SouOWendel) <souowendel@gmail.com>
2024-08-21 03:25:49 -0300

    feat(pokedex): created a button with click listener for load more pokemons

--- javascript-studies/pokedex/assets/js/main.js
+++ javascript-studies/pokedex/assets/js/main.js
@@ -1,20 +1,30 @@
-function convertPokemonToHtml(pokemon) {
-	return `
-		<li class="pokemon ${pokemon.type}">
-				<span class="number">#0${pokemon.pokeNumber}</span>
-				<span class="name">${pokemon.name}</span>
-				<div class="details">
-					<ol class="types">
-						${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-					</ol>
-					<img src="${pokemon.img}"
-					alt="${pokemon.name}">
-				</div>
-			</li>
-	`;
+const loadMoreButton = document.getElementById('loadMoreButton');
+const pokemonOl = document.getElementById('pokemonList');
+const pagination = {
+	limit: 5,
+	offset: 0
+};
+
+function loadPokemonItems(offset, limit) {
+	pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+		pokemonOl.innerHTML += pokemons.map((pokemon) => 
+		`<li class="pokemon ${pokemon.type}">
+			<span class="number">#0${pokemon.pokeNumber}</span>
+			<span class="name">${pokemon.name}</span>
+			<div class="details">
+				<ol class="types">
+					${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+				</ol>
+				<img src="${pokemon.img}"
+				alt="${pokemon.name}">
+			</div>
+		</li>`).join('');
+	});
 }
 
-const pokemonOl = document.getElementById('pokemonList');
-pokeApi.getPokemons().then((pokemons = []) => {
-	pokemonOl.innerHTML = pokemons.map(convertPokemonToHtml).join('');
+loadPokemonItems(pagination.offset, pagination.limit);
+
+loadMoreButton.addEventListener('click', () => {
+	pagination.offset += pagination.limit;
+	loadPokemonItems(pagination.offset, pagination.limit);
 });
